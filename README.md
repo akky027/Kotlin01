019031bfc702639428b24540e7882e242bdc47ee
akky027 <akiyama@rightcode.co.jp>
2025-06-16 16:39:02 +0900

    セッティング

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Kotlin01
+
+起動コマンド
+
+./gradlew bootRun
+
+閉じる時
+
+control + C
+
+
+
+テストパス
+
+http://localhost:8080/hello
---
+++ src/main/kotlin/com/api/kotlin01/User.kt
@@ -0,0 +1,7 @@
+package com.api.kotlin01
+
+data class User(
+    val id: Long,
+    val name: String,
+    val email: String
+)
